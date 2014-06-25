062b23d13ba9ac0627f6a35c8a8ddf84e238bd50
Tim Glaser <tim.glaser@ecotrust.org>
2014-06-24 18:03:11 -0700

    'fab vagrant backup_db' works now.

--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,10 @@ CHEF_VERSION = '10.20.0'
 
 branch = 'master'
 project = "wagtaildemo"
-app = ""
+app = "wagtaildemo"
+
+env.code_dir = '~/%s' % (app)
+
 
 env.root_dir = "/usr/local/apps/%s" % project
 env.venvs = '/usr/local/venv'
@@ -73,97 +76,37 @@ def set_env_for_user(user='example'):
             raise Exception("Missing data from ssh-config")
 
 
-@task
-def up():
-    """
-    Provision with Chef 11 instead of the default.
+# @task
+# def loaddata(fname='demo/fixtures/initial_data.json'):
+#     """
+#     Loads data from local fixtures file. By defult it loads apps/survey/fixtures/surveys.json.gz
+#     You can use 'fab vagrant loaddata:PATH_TO_FIXTURE_FILE' to load a specific file.
+#     """
+#     set_env_for_user('vagrant')
+#     with cd(env.code_dir):
+#         with _virtualenv():
+#             _manage_py('loaddata %s' %(fname))
 
-    1.  Bring up VM without provisioning
-    2.  Remove all Chef and Moneta
-    3.  Install latest Chef
-    4.  Reload VM to recreate shared folders
-    5.  Provision
-    """
-    local('vagrant up --no-provision')
 
-    set_env_for_user('vagrant')
-
-    sudo('gem uninstall --no-all --no-executables --no-ignore-dependencies chef moneta')
-    install_chef(latest=False)
-    local('vagrant reload')
-    local('vagrant provision')
-
+# @task
+# def dumpdata(fname='data.json'):
+#     with cd('/home/vagrant/wagtaildemo/'):
+#         run('ls')
+#         _manage_py('dumpdata --format=json --indent=4  wagtailcore.page demo.multilingualpage demo.langrootpage demo.spanishhomepage demo.englishhomepage > backups/%s' % (fname))
 
 @task
-def bootstrap(username=None):
-    set_env_for_user(username)
-
-    # Bootstrap
-    #run('test -e %s || ln -s /vagrant/marco %s' % (env.code_dir, env.code_dir))
-    with cd(env.code_dir):
-        with _virtualenv():
-            sudo('pip install -r requirements/dev.txt')
-            _manage_py('syncdb --noinput')
-            # _manage_py('add_srid 99996')
-            _manage_py('migrate')
-            # _manage_py('collectstatic --noinput')
-            # _manage_py('enable_sharing')
-
-
-@task
-def createsuperuser(username=None):
-    set_env_for_user(username)
-
-    # Bootstrap
-    #run('test -e %s || ln -s /vagrant/marco %s' % (env.code_dir, env.code_dir))
-    with cd(env.code_dir):
-        with _virtualenv():
-            _manage_py('createsuperuser')
-
-
-@task
-def runserver():
-    set_env_for_user('vagrant')
-    with cd(env.code_dir):
-        with _virtualenv():
-            _manage_py('runserver 0.0.0.0:8000')
-
-
-@task
-def loaddata(fname='apps/survey/fixtures/surveys.json.gz'):
-    """
-    Loads data from local fixtures file. By defult it loads apps/survey/fixtures/surveys.json.gz
-    You can use 'fab vagrant loaddata:PATH_TO_FIXTURE_FILE' to load a specific file.
-    """
-
-    set_env_for_user('vagrant')
-    with cd(env.code_dir):
-        with _virtualenv():
-            _manage_py('loaddata %s' %(fname))
-
-
-@task
-def dumpdata(fname='data.json'):
-    _manage_py('dumpdata --format=json --indent=4  wagtailcore.page demo.multilingualpage demo.langrootpage demo.spanishhomepage demo.englishhomepage > backups/%s' % (fname))
-
-    # with cd(env.code_dir):
-    #     with _virtualenv():
-            # _manage_py('dumpdata --format=json --indent=4  wagtailcore.page demo.langrootpage demo. > demo/fixtures/langroot_only.json | gzip > apps/survey/fixtures/%s' %(fname))
-            # get('apps/survey/fixtures/%s' %(fname), 'backups/%s' %(fname))
-
-@task
-def backup_db():
+def backup_db(dbname='wagtaildemo'):
     # pg_dump wagtaildemo -n public -c -f backups/____FILENAME____ -Fc -O -no-acl -U postgres
     date = datetime.datetime.now().strftime("%Y-%m-%d%H%M")
-    dump_name = "%s-geosurvey.dump" % date
-    run("pg_dump geosurvey -n public -c -f /tmp/%s -Fc -O -no-acl -U postgres" % dump_name)
+    dump_name = "%s-%s.dump" % (date, dbname)
+    run("pg_dump %s -n public -c -f /tmp/%s -Fc -O -no-acl -U postgres" % (dbname, dump_name))
     get("/tmp/%s" % dump_name, "backups/%s" % dump_name)
 
 @task
 def restore_db(dump_name):
     # pg_restore --verbose --clean --no-acl --no-owner -U postgres -d wagtaildemo backups/____FILENAME____
     put(dump_name, "/tmp/%s" % dump_name.split('/')[-1])
-    run("pg_restore --verbose --clean --no-acl --no-owner -U postgres -d geosurvey /tmp/%s" % dump_name.split('/')[-1])
+    run("pg_restore --verbose --clean --no-acl --no-owner -U postgres -d wagtaildemo /tmp/%s" % dump_name.split('/')[-1])
     #run("cd %s && %s/bin/python manage.py migrate --settings=config.environments.staging" % (env.app_dir, env.venv))
 
 
