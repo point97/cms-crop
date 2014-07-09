17e494ab66bb0b689faf759905d1a08d9dda3563
Tim Glaser <tim.glaser@ecotrust.org>
2014-07-08 17:35:22 -0700

    Add AnimateScrollTo directive. Refs #45.

--- demo/static/demo/js/app.js
+++ demo/static/demo/js/app.js
@@ -1,19 +1,2 @@
-angular.module('cropApp', ['ngRoute'] )
-    .config(function($httpProvider, $routeProvider, $locationProvider) {
-        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-    
-        $routeProvider
-           .when('/', {
-            templateUrl: ' ',
-            controller: 'HomPageCtrl',
-          })
-           .when('/:section', {
-            template: ' ',
-            controller: 'HomPageCtrl',
-          })
-           .when('/:section/:topic', {
-            template: ' ',
-            controller: 'HomPageCtrl',
-          })
 
-});
+angular.module('cropApp', []);
--- demo/static/demo/js/main.js
+++ demo/static/demo/js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-    
-
     //Apply img-thumbnail class to body-content images
     $('.body-content img').addClass("img-thumbnail");
+});
+
+
+/**
+ * Animate scrolling to mid-page anchors on initial load.
+ */
+$(document).ready(function () {
+    var hash = window.location.hash,
+        speed = 300;
+    if (hash !== '') {
+      //$('html,body').animate({scrollTop: $(hash).offset().top}, speed);
+    }
 });
