cff4ba640dc5b31e9ac34f371b7ca936d2f337fb
Tim Glaser <tim.glaser@ecotrust.org>
2014-07-09 10:56:23 -0700

    Get carousels working again and move our carousel directive. Refs #45.

--- demo/static/demo/js/main.js
+++ demo/static/demo/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    /* Initialise bxSlider */
+    $('.not-explore-carousel .bxslider').bxSlider({
+        captions: true
+    });
+});
+
+$(document).ready(function(){
     //Apply img-thumbnail class to body-content images
     $('.body-content img').addClass("img-thumbnail");
 });
