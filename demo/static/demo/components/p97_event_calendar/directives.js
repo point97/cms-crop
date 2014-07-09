adeca0897fd94e6f5352f0c430b81a7af809e6e1
Wil Black <wilblack21@gmail.com>
2014-07-09 14:40:13 -0700

    Adding in event list directive.

--- demo/static/demo/components/p97_event_calendar/directives.js
+++ demo/static/demo/components/p97_event_calendar/directives.js
@@ -1,24 +1,56 @@
 angular.module('cropApp')
     .directive('eventcalendar', function(){
 
-    function link(scope, elt, attr){
-        
-    };
 
     function calCtrl($scope){
+        $scope.cal = {}
+        $scope.cal.active_date = '';
 
         $scope.today = function() {
             $scope.dt = new Date();
         };
         $scope.today();
         $scope.minDate = $scope.dt;
+        $scope.cal.active_date = $scope.dt;
+
+        // Get the first 5 events.
+        $scope.events = [
+            {
+                date_from:'2014-07-17', 
+                date_to:'2014-07-18', 
+                title:'Title goes here', 
+                short_description :'Short description goes here', 
+                location:'It happens here', 
+                body: "Body text goes here."
+            },{
+                date_from:'2014-07-19', 
+                date_to:'2014-07-18', 
+                title:'Title goes here 2', 
+                short_description :'Short description goes here 2', 
+                location:'It happens here 2', 
+                body: "Body text goes here. 2"
+            }
+        ];
+
         
     }
 
     return {
         templateUrl: "/static/demo/components/p97_event_calendar/views/event_calendar.html",
         scope :{},
-        link:link,
         controller:calCtrl
+    };
+}).directive('eventslist', function(){
+    return {
+        require: '^eventcalendar',
+        restrict: 'AE',
+        transclude: true,
+        scope: {
+            date: "=",
+            events: "="
+        },
+        link: function(scope, element, attrs) {
+        },
+        templateUrl: '/static/demo/components/p97_event_calendar/views/events_list.html'
     };
 });
