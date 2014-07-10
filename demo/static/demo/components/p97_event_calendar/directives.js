0073031c28fef995a675420808c4f5815e1925ba
Wil Black <wilblack21@gmail.com>
2014-07-10 09:30:34 -0700

    Got a .cal.range that updates whenver the users interacts with the calendar. Added Date.js EN ans ES js libraries. Adjust calnedar styles.

--- demo/static/demo/components/p97_event_calendar/directives.js
+++ demo/static/demo/components/p97_event_calendar/directives.js
@@ -1,14 +1,23 @@
+/*
+    This requires Date.js
+*/
+
 angular.module('cropApp')
     .directive('eventCalendar', function($http){
 
     function calCtrl($scope, $http){
         $scope.cal = {}
         $scope.cal.active_date = '';
+        
+        // This is the range used to build the API queryy string.
+        // Leave second entry null for single day queries
+        $scope.cal.range = [null,null]; 
 
         $scope.today = function() {
             $scope.dt = new Date();
         };
         $scope.today();
+        
         $scope.minDate = $scope.dt;
         $scope.cal.active_date = $scope.dt;
 
@@ -23,8 +32,24 @@ angular.module('cropApp')
                     console.log("Opps we errored out on events.")
                 });
         }
-        
         $scope.get_events('');
+
+        // Add watcher for month change and update cal.range to the slected month
+        $scope.$watch(function(){
+            return $("thead tr:nth-child(1) th:nth-child(2) strong").text();
+        }, function(newValue){
+            // Calendar Month change
+            startDate = Date.parse(newValue);
+            endDate = Date.parse(newValue).moveToLastDayOfMonth();
+            $scope.cal.range = [startDate,endDate];
+        });
+
+        // A watcher for user selected date, dt. Updates cal.range to a one date query.
+        $scope.$watch('dt', function(newValue){
+            date = Date.parse(newValue);
+            $scope.cal.range = [newValue, null];
+        });
+
     }
 
     return {
@@ -46,11 +71,11 @@ angular.module('cropApp')
         require: '^eventCalendar',
         restrict: 'AE',
         transclude: true,
-        scope: {
-            date: "=",
-            events: "="
-        },
-        link: function(scope, element, attrs) {
+        // scope: {
+        //     date: "=",
+        //     events: "="
+        // },
+        link: function(scope, element, attrs, calCtrl) {
         },
         templateUrl: '/static/demo/components/p97_event_calendar/views/events_list.html'
     };
